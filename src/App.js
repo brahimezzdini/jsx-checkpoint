cef9a80630249060a0e65819344d83795380ad24
unknown <ezzdinib@gmail.com>
2021-05-29 10:59:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import imageInSrc from './imageInSrc.png';
+import "./App.css";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+<div style={{border:'solid 1px black' , maxWidth:'99vw'}}>
+
+ <h1 class="title red">Brahim Ezzdini</h1>
+
+ <br/>
+
+ <img src={imageInSrc} />
+
+ <br />
+ <br/>
+
+ <img src="/imageInPublic.png" />
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+
+</div>
+
+
   );
 }
 
